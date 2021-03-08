7e7b36b535cc1887b4136a1770b8adaee8098117
Akmal Adnan <68601221+akmal578@users.noreply.github.com>
2021-03-09 01:49:53 +0800

    cleanup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -61,9 +61,7 @@ const Login = (props) => {
       {Object.keys(errors).length > 0 && (
         <div className="ui error message">
           <ul className="list">
-            {Object.values(errors).map((value) => (
-              <li key={value}>{value}</li>
-            ))}
+            <li>{errors}</li>
           </ul>
         </div>
       )}
